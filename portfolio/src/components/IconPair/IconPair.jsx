4698afc66e2f6ef02129d33e25ee34c1cf720ca2
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-10-17 18:15:52 +0300

    new files sistem

--- portfolio/src/components/IconPair/IconPair.jsx
+++
@@ -1,7 +0,0 @@
-export default function IconPair (props) {
-    return (
-        <div className="icons">
-            {props.icons}
-        </div>
-    )
-}
--- portfolio/src/components/Title/Title.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './title.scss'
-
-export default function Uptitle (props) {
-    return(
-        <h2 className="title">{props.text}</h2>
-    )
-}
